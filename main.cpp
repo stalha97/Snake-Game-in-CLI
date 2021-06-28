dd850a2ba01bf43b5b3c2866e801eef004d69cc7
stalha97 <stalha97@gmail.com>
2021-06-28 17:17:58 +0500

    Changed all the namings according to naming conventions

--- main.cpp
+++ main.cpp
@@ -5,22 +5,21 @@
 
 using namespace std;
 
-#define initial_food_x 15
-#define initial_food_y 19
+#define INITIAL_FOOD_X 15
+#define INITIAL_FOOD_Y 19
 
-#define initial_snake_tail_x 5
-#define initial_snake_head_x 10
+#define INITIAL_SNAKE_TAIL_X 5
+#define INITIAL_SNAKE_HEAD_X 10
 
-#define initial_snake_tail_y 10
-#define initial_snake_head_y 10
+#define INITIAL_SNAKE_TAIL_Y 10
+#define INITIAL_SNAKE_HEAD_Y 10
 
-#define playground_x_width 40
-#define playground_y_height 20
-#define playground_size (playground_x_width*playground_y_height)
+#define PLAYGROUND_X_WIDTH 40
+#define PLAYGROUND_Y_HEIGHT 20
+#define PLAYGROUND_SIZE (PLAYGROUND_X_WIDTH*PLAYGROUND_Y_HEIGHT)
 
-#define snake_tail_loc 0
-#define snake_tail_adjacent 1
-#define initial_snake_head_location_in_array 1
+#define SNAKE_TAIL_LOC 0
+#define SNAKE_TAIL_ADJACENT 1
 
 typedef struct Point{
     int x;
@@ -31,195 +30,188 @@ enum MovementLane {X_LANE, Y_LANE};
 enum Direction { UP,DOWN,LEFT,RIGHT};
 
 // helper functions
-MovementLane calculate_snake_lane(int startingPoint);
-int length_between_2_cordinates(int startingPoint);
-void gotoxy(int,int);
+MovementLane calculateSnakeLane(int startingPoint);
+int LengthBetween2Cordinates(int startingPoint);
+void GoToXY(int,int);
 
 // Game functions
-void draw_ground();
-void check_food_status();
-void draw_snake();
-void clear_snake();
-void draw_snake_between_2_points(int startingPoint);
-void clear_snake_between_2_points(int startingPoint);
-void update_tail();
-void Alive_or_dead();
-void take_input();
+void DrawGround();
+void CheckFoodStatus();
+void DrawSnake();
+void ClearSnake();
+void DrawSnakeBetween2Points(int startingPoint);
+void ClearSnakeBetween2Points(int startingPoint);
+void UpdateTail();
+void AliveOrDead();
+void TakeInput();
 
 //INITIAL STARTING VALUES
-Point food = {initial_food_x, initial_food_y};
+Point food = {INITIAL_FOOD_X, INITIAL_FOOD_Y};
 
-Point snakeTurningPoints[playground_size] = {
-    {initial_snake_tail_x,initial_snake_tail_y},
-    {initial_snake_head_x,initial_snake_head_y},
+Point snakeTurningPoints[PLAYGROUND_SIZE] = {
+    {INITIAL_SNAKE_TAIL_X,INITIAL_SNAKE_TAIL_Y},
+    {INITIAL_SNAKE_HEAD_X,INITIAL_SNAKE_HEAD_Y},
 };
 
-int head= initial_snake_head_location_in_array;
+int head = 1;
 Direction snakeTailMovingDirection;
 MovementLane snakeCalculatedLane;
 
 int m=4,score=0;
 
-int   flag, Length_Between_2_Cordinates,  check,        Length_Sign;
-bool  InputFailed=true,breakk=false;
+int   flag, lengthBetween2Cordinates, check, lengthSign;
+bool  inputFailed=true,breakk=false;
 char  show,restart;
 
 int main()
 {
-    draw_ground();
+    DrawGround();
     while(1)
     {
-        check_food_status();
-        draw_snake();
-        clear_snake();
-        update_tail();
-        Alive_or_dead();
+        CheckFoodStatus();
+        DrawSnake();
+        ClearSnake();
+        UpdateTail();
+        AliveOrDead();
         if(breakk==true)
         {
             break;
         }
-        take_input();
+        TakeInput();
     }
 
     _sleep(2500);
     system("cls");
 
-    draw_ground();
-    gotoxy(5,10);
+    DrawGround();
+    GoToXY(5,10);
     cout<<"GAME OVER";
-    gotoxy(5,11);
+    GoToXY(5,11);
     cout<<"YOUR SCORE IS:   "<<score;
     _sleep(5000);
 
     return 0;
 }
 //****************************************************************
-void draw_ground()
+void DrawGround()
 {
-    for(int y=0; y<=playground_y_height; y++)
+    for(int y=0; y<=PLAYGROUND_Y_HEIGHT; y++)
     {
-        for(int x=0; x<=playground_x_width; x++)
+        for(int x=0; x<=PLAYGROUND_X_WIDTH; x++)
         {
             // Draw * on border pixels
-            if(x==0 || x==playground_x_width || y==0 || y==playground_y_height)
-            {
+            if(x==0 || x==PLAYGROUND_X_WIDTH || y==0 || y==PLAYGROUND_Y_HEIGHT)
                 cout<<'*';
-            }
             // Draw empty space on non-border pixels
             else
-            {
                 cout<<" ";
-            }
         }
         cout<<endl;
     }
 }
 //*******************************************************************
-MovementLane calculate_snake_lane(int startingPoint){
+MovementLane calculateSnakeLane(int startingPoint){
     // Determine direction of snake here
     // x coordinates of adjacent values are same => snake moving in direction of x
     if((snakeTurningPoints[startingPoint+1].x - snakeTurningPoints[startingPoint].x) ==0)
     {
         return MovementLane::Y_LANE;
     }
+
     // y coordinates of adjacent values are same => snake moving in direction of y
-    else if((snakeTurningPoints[startingPoint+1].y - snakeTurningPoints[startingPoint].y) ==0)
-    {
-        return MovementLane::X_LANE;
-    }
+    return MovementLane::X_LANE;
+
 }
 
-int length_between_2_cordinates(int startingPoint){
-    if(calculate_snake_lane(startingPoint) == MovementLane::Y_LANE){
+int LengthBetween2Cordinates(int startingPoint){
+    if(calculateSnakeLane(startingPoint) == MovementLane::Y_LANE){
         return snakeTurningPoints[startingPoint+1].y - snakeTurningPoints[startingPoint].y;
     }
-    else{
-        return snakeTurningPoints[startingPoint+1].x - snakeTurningPoints[startingPoint].x;
-    }
+
+    return snakeTurningPoints[startingPoint+1].x - snakeTurningPoints[startingPoint].x;
 }
 
-void draw_snake()
+void DrawSnake()
 {
-    for(int j=0; j<head; j++)
+    for(int coordinateNumber=0; coordinateNumber<head; coordinateNumber++)
     {
-        snakeCalculatedLane = calculate_snake_lane(j);
-        Length_Between_2_Cordinates = length_between_2_cordinates(j);
-        draw_snake_between_2_points(j);
+        snakeCalculatedLane = calculateSnakeLane(coordinateNumber);
+        lengthBetween2Cordinates = LengthBetween2Cordinates(coordinateNumber);
+        DrawSnakeBetween2Points(coordinateNumber);
     }
     _sleep(90);
 }
-void clear_snake(){
+void ClearSnake(){
 
-    for(int j=0; j<head; j++)
+    for(int coordinateNumber=0; coordinateNumber<head; coordinateNumber++)
     {
-        snakeCalculatedLane = calculate_snake_lane(j);
-        Length_Between_2_Cordinates = length_between_2_cordinates(j);
-        clear_snake_between_2_points(j);
+        snakeCalculatedLane = calculateSnakeLane(coordinateNumber);
+        lengthBetween2Cordinates = LengthBetween2Cordinates(coordinateNumber);
+        ClearSnakeBetween2Points(coordinateNumber);
     }
 }
 
 //**********************************************************
-void draw_snake_between_2_points(int startingPoint)
+void DrawSnakeBetween2Points(int startingPoint)
 {
     int i=0;
     while(1)
     {
         // Goto each Pixel with increments
         if(snakeCalculatedLane == X_LANE)
-            gotoxy(snakeTurningPoints[startingPoint].x+i ,snakeTurningPoints[startingPoint].y);
+            GoToXY(snakeTurningPoints[startingPoint].x+i ,snakeTurningPoints[startingPoint].y);
         else if(snakeCalculatedLane == Y_LANE)
-            gotoxy(snakeTurningPoints[startingPoint].x, snakeTurningPoints[startingPoint].y+i);
+            GoToXY(snakeTurningPoints[startingPoint].x, snakeTurningPoints[startingPoint].y+i);
 
         // Display Head Symbol if it is last line ( head-1 to head) and it is the last point(head)
-        if(startingPoint==head-1 && i==Length_Between_2_Cordinates)
+        if(startingPoint==head-1 && i==lengthBetween2Cordinates)
             cout<<'$';
         // Display Body Symbol
         else
             cout<<'O';
 
         // Break if all the pixels between 2 points are drawn
-        if(Length_Between_2_Cordinates>0){
+        if(lengthBetween2Cordinates>0){
             i++;
-            if(i==Length_Between_2_Cordinates+1) break;
+            if(i==lengthBetween2Cordinates+1) break;
         }
-        if(Length_Between_2_Cordinates<0){
+        if(lengthBetween2Cordinates<0){
             i--;
-            if(i==Length_Between_2_Cordinates-1) break;
+            if(i==lengthBetween2Cordinates-1) break;
         }
     }
 }
-void clear_snake_between_2_points(int startingPoint)
+void ClearSnakeBetween2Points(int startingPoint)
 {
-
     int i=0;
     while(1)
     {
         // Goto each Pixel with increments
         if(snakeCalculatedLane == X_LANE)
-            gotoxy(snakeTurningPoints[startingPoint].x+i ,snakeTurningPoints[startingPoint].y);
+            GoToXY(snakeTurningPoints[startingPoint].x+i ,snakeTurningPoints[startingPoint].y);
         else if(snakeCalculatedLane == Y_LANE)
-            gotoxy(snakeTurningPoints[startingPoint].x, snakeTurningPoints[startingPoint].y+i);
+            GoToXY(snakeTurningPoints[startingPoint].x, snakeTurningPoints[startingPoint].y+i);
 
         // Clear snake
         cout<<' ';
 
         // Break if all the pixels between 2 points are drawn
-        if(Length_Between_2_Cordinates>0){
+        if(lengthBetween2Cordinates>0){
             i++;
-            if(i==Length_Between_2_Cordinates+1) break;
+            if(i==lengthBetween2Cordinates+1) break;
         }
-        if(Length_Between_2_Cordinates<0){
+        if(lengthBetween2Cordinates<0){
             i--;
-            if(i==Length_Between_2_Cordinates-1) break;
+            if(i==lengthBetween2Cordinates-1) break;
         }
     }
 }
 
 //*************************************************
-void update_tail()
+void UpdateTail()
 {
-    MovementLane snakeTailLane = calculate_snake_lane(0);
-    int lengthBetweenTailAdjacents = length_between_2_cordinates(0);
+    MovementLane snakeTailLane = calculateSnakeLane(0);
+    int lengthBetweenTailAdjacents = LengthBetween2Cordinates(0);
 
     if(snakeTailLane == MovementLane::X_LANE){
         if(lengthBetweenTailAdjacents > 0){
@@ -263,17 +255,17 @@ void update_tail()
             snakeTurningPoints[i].y = snakeTurningPoints[i+1].y;
         }
 
-        // Updated head location and J value
+        // Updated head location
         head--;
     }
 
 }
 //******************************************************
-void take_input()
+void TakeInput()
 {
     if(_kbhit())
     {
-        InputFailed=false;
+        inputFailed=false;
         snakeTurningPoints[head+1].x = 0;
         snakeTurningPoints[head+1].y = 0;
         char c=getch();
@@ -296,10 +288,10 @@ void take_input()
         }
         else
         {
-            InputFailed=true;
+            inputFailed=true;
         }
 
-        if(InputFailed==false)
+        if(inputFailed==false)
         {
             head++;
             snakeTurningPoints[head].x = snakeTurningPoints[head].x + snakeTurningPoints[head-1].x;
@@ -326,7 +318,7 @@ void take_input()
 }
 //***********************************************************************
 
-void check_food_status()
+void CheckFoodStatus()
 {
     if(snakeTurningPoints[head].x == food.x && snakeTurningPoints[head].y == food.y)
     {
@@ -348,17 +340,17 @@ void check_food_status()
         }
 
         srand(time(NULL));
-        food.x=rand()% (playground_x_width-1) + 1;
-        food.y=rand()% (playground_y_height-1) + 1;
+        food.x=rand()% (PLAYGROUND_X_WIDTH-1) + 1;
+        food.y=rand()% (PLAYGROUND_Y_HEIGHT-1) + 1;
 
         score++;
     }
 
-    gotoxy(food.x,food.y);
+    GoToXY(food.x,food.y);
     cout<<'%';
 }
 
-void Alive_or_dead()
+void AliveOrDead()
 {
     for(int i=0; i<head-1; i++)
     {
@@ -392,16 +384,16 @@ void Alive_or_dead()
     }
 
     if(snakeTurningPoints[head].x==0 ||
-        snakeTurningPoints[head].x==playground_x_width ||
+        snakeTurningPoints[head].x==PLAYGROUND_X_WIDTH ||
         snakeTurningPoints[head].y==0 ||
-        snakeTurningPoints[head].y==playground_y_height)
+        snakeTurningPoints[head].y==PLAYGROUND_Y_HEIGHT)
     {
         breakk=true;
     }
 }
 //***********************************************************************
 
-void gotoxy(int cordx,int cordy)
+void GoToXY(int cordx,int cordy)
 {
     COORD coord= {cordx,cordy};
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
